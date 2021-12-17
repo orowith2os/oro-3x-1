e9acfd1d612b6a7d39384f73cb8d93bb84acbdc0
Oro <93224879+orowith2os@users.noreply.github.com>
2021-12-17 21:56:16 +0000

    added the ability to accept user input

--- 3x+1.py
+++ 3x+1.py
@@ -1,7 +1,7 @@
 import random
-list = random.randint(0,10000000000000000000000000000000)
-while (list > 1):
-  if (list % 2 == 0): list = list / 2
-  else: list = 3 * list + 1
-  print(list)
-  print()
+print("Type any positive integer, then press Enter:")
+list = int (input(""))
+while (list != 1):
+ if (list % 2 == 0): list = list / 2
+ else: list = 3 * list + 1
+ print(list)
