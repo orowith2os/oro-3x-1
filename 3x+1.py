122e127543759422bfe5b5eabab4e5248034a235
Oro <93224879+orowith2os@users.noreply.github.com>
2021-12-17 20:17:08 +0000

    added import random

--- 3x+1.py
+++ 3x+1.py
@@ -1,4 +1,5 @@
-list = random.randint(0,1000000000000)
+import random
+list = random.randint(0,10000000000000000000000000000000)
 while (list > 1):
   if (list % 2 == 0): list = list / 2
   else: list = 3 * list + 1
--- README.md
+++
@@ -1,2 +0,0 @@
-# oro-3x-1
-Python script that runs 3x+1 if a number is odd, or divides by 2 if even
